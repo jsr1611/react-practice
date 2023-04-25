17b0be017efd87ba1b16e164c2df3cbfb23f453f
Jumanazar <jsr1611@outlook.com>
2023-04-25 16:51:22 +0900

    updated axios interceptor req and resp

--- react-standard/src/hook/useAxiosInterceptor.tsx
+++ react-standard/src/hook/useAxiosInterceptor.tsx
@@ -1,8 +1,27 @@
+import client from "@/plugin/client";
 import { useEffect } from "react";
 
 /**
  * @description async interceptor hook
  */
-function useAxiosInterceptor() {}
+function useAxiosInterceptor() {
+    useEffect(() => {
+        // req
+        const reqInterceptor = client.interceptors.request.use(
+            (config) => {
+                console.log("async request");
+
+                return config;
+            },
+            (error) => {
+                console.log("async request error");
+
+                Promise.reject(error);
+            }
+        );
+
+        // resp
+    }, []);
+}
 
 export default useAxiosInterceptor;
